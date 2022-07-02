a4885e896f7a396517369335ec2089dea9dacc3b
Lucas Alves <lucasdolohov@gmail.com>
2022-07-02 15:11:25 -0300

    feat: models

---
+++ src/models/tasksModel.ts
@@ -0,0 +1,20 @@
+import connection from "./connection"
+
+export const getAll = async (): Promise<ITaskOnDB[]> => {
+  const [result] = await connection.execute(
+    'SELECT * FROM Ebyrt.Tasks ORDER BY name'
+  )
+  return result as ITaskOnDB[];
+}
+
+export const createTask = async (task: ITask) => {
+  
+}
+
+export const removeTask = async (id: number) => {
+  
+}
+
+export const updateTask = async (id: number, taks: ITask) => {
+  
+}
